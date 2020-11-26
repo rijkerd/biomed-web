798848ee5a09ca00771295c4b4899d3addb70df3
rijkerd <richardaggrey7@gmail.com>
2020-11-26 15:38:12 +0300

    refactor: width mobile and authentication

--- src/state/actions/auth.js
+++ src/state/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import pick from 'lodash/pick';
 import * as actionTypes from './actionTypes';
 import { dispatch as storeDispatch } from '../store';
 import { wrappedModules } from './moduleAction';
@@ -24,36 +25,37 @@ export const authFail = (error) => {
 };
 
 export const logout = () => (dispatch) => {
-  axios.post(`${process.env.REACT_APP_BASE_URL}/rest-auth/logout/`).then(() => {
-    localStorage.removeItem('token'); // eslint-disable-line no-undef
-    localStorage.removeItem('expirationDate'); // eslint-disable-line no-undef
+  // eslint-disable-next-line no-undef
+  const token = localStorage.getItem('access_token');
+  axios
+    .post(
+      `${process.env.REACT_APP_BASE_URL}/auth/token/logout/`,
+      {},
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }
+    )
+    .then(() => {
+      localStorage.removeItem('access_token'); // eslint-disable-line no-undef
 
-    dispatch({ type: actionTypes.AUTH_LOGOUT });
-  });
+      dispatch({ type: actionTypes.AUTH_LOGOUT });
+    });
 };
 
 export const wrappedLogout = () => storeDispatch(logout());
-
-export const checkAuthTimeout = (expirationTime) => {
-  return (dispatch) => {
-    setTimeout(() => {
-      dispatch(logout());
-    }, expirationTime * 1000);
-  };
-};
 
 export const authLogin = (credentials, onSuccess, onError) => {
   return (dispatch) => {
     dispatch(authStart());
     axios
-      .post(`${process.env.REACT_APP_BASE_URL}/rest-auth/login/`, credentials)
+      .post(`${process.env.REACT_APP_BASE_URL}/auth/token/login/`, credentials)
       .then((res) => {
-        const token = res.data.key;
-        const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
-        localStorage.setItem('token', token); // eslint-disable-line no-undef
-        localStorage.setItem('expirationDate', expirationDate); // eslint-disable-line no-undef
+        const token = res.data.auth_token;
+        // eslint-disable-next-line no-undef
+        localStorage.setItem('access_token', token);
         dispatch(authSuccess(token));
-        dispatch(checkAuthTimeout(3600));
         wrappedModules();
         onSuccess();
       })
@@ -78,36 +80,12 @@ export const authSignup = (username, email, password1, password2) => {
         password2,
       })
       .then((res) => {
-        const token = res.data.key;
-        const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+        const token = pick(res, ['auth_token']);
         localStorage.setItem('token', token); // eslint-disable-line no-undef
-        localStorage.setItem('expirationDate', expirationDate); // eslint-disable-line no-undef
         dispatch(authSuccess(token));
-        dispatch(checkAuthTimeout(3600));
       })
       .catch((err) => {
         dispatch(authFail(err));
       });
-  };
-};
-
-export const authCheckState = () => {
-  return (dispatch) => {
-    const token = localStorage.getItem('token'); // eslint-disable-line no-undef
-    if (token === undefined) {
-      dispatch(logout());
-    } else {
-      const expirationDate = new Date(localStorage.getItem('expirationDate')); // eslint-disable-line no-undef
-      if (expirationDate <= new Date()) {
-        dispatch(logout());
-      } else {
-        dispatch(authSuccess(token));
-        dispatch(
-          checkAuthTimeout(
-            (expirationDate.getTime() - new Date().getTime()) / 1000
-          )
-        );
-      }
-    }
   };
 };
